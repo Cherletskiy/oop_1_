ee417fefcd4c4d17c8d887607123befeff92321a
Vladimir Cherletskiy <cherletskiy1337@mail.ru>
2024-12-03 12:27:49 +0300

    second task

--- main.py
+++ main.py
@@ -10,12 +10,39 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def set_rates(self, lecturer, course, grade):
+        if not course in self.courses_in_progress + self.finished_courses:
+            raise ValueError("Студент не был на данном курсе")
+        if course not in lecturer.courses_attached:
+            raise ValueError("Данный лектор не ведёт этот курс")
+        if type(grade) != int or grade not in range(1, 11):
+            raise TypeError("Оценка должны быть целым числом от 1 до 10")
+        lecturer.update_rating()
+        lecturer.grades[course] += [grade]
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    CLASS_TYPE = "lecturer"
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def update_rating(self):
+        for course in self.courses_attached:
+            if course not in self.grades:
+                self.grades[course] = []
+
+
+class Reviewer(Mentor):
+    CLASS_TYPE = "reviewer"
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -24,13 +51,8 @@ class Mentor:
             else:
                 student.grades[course] = [grade]
         else:
-            return 'Ошибка'
+            raise ValueError("Ошибка в оценке")
 
 
-class Lecturer(Mentor):
-    pass
 
-
-class Reviewer(Mentor):
-    pass
 
